dd46687d9bfbde245279c9a9ed718183b0da2b82
DewofyourYouth <jacobshore@gmail.com>
2020-10-08 15:39:34 +0300

    make some routes

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
-package rest_test
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "it works!",
+		})
+	})
+
+	r.GET("/hello/:name", func(context *gin.Context) {
+		name := context.Param("name")
+		context.String(http.StatusOK, "Hello %s!", name)
+	})
+
+	err := r.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
